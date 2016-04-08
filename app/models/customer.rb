4e2cf860360c04789d6a709de08a249219eb724c
Bergson Lima <bergsonsud@gmail.com>
2016-04-08 00:48:10 -0300

    Concluindo view com informações do cliente

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,24 @@
 class Customer < ActiveRecord::Base
 	self.per_page = 20
+
+	def address
+		"#{logradouro}, #{numero} #{complemento} #{bairro} - #{municipio}-#{estado}"		
+	end
+
+	def phone_numbers
+		"#{telefone} #{telefone2} #{telefone3}"
+	end
+
+	def mobilephone_numbers
+		"#{celular} #{celular2}"
+	end	
+
+	def mail
+		"#{email} #{email2}"		
+	end
+
+	def since
+		"#{desde.strftime("%d/%m/%Y")}"
+	end
+
 end
