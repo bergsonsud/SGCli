eb853d496cf2d535d76e497edfee94166eb0c2d8
Bergson Lima <bergsonsud@gmail.com>
2016-05-01 18:39:13 -0300

    Finalizado view de usuarioios"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,15 +41,17 @@ Rails.application.configure do
   
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :authentication => :login,
-    :user_name => "syssgcli@gmail.com",
-    :password => "Sgcli123456"
-  }
+  
 
+
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "syssgcli@gmail.com",
+ password: "Sgcli123456"
+}
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
